5a05e93f22cca7e83c38a81d58c2eee8f22d4d48
NTLuka <noobtenluka@gmail.com>
2021-12-09 16:00:19 +0100

    unfinished solution for day 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day01;
 mod day02;
+mod day03;
 mod day06;
 mod day08;
 
@@ -15,6 +16,7 @@ fn main() {
     match day {
         1 => day01::run(),
         2 => day02::run(),
+        3 => day03::run(),
         6 => day06::run(),
         8 => day08::run(),
         _ => panic!("Either not a valid day or not implemented yet!"),
