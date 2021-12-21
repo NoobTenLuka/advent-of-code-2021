7cc010f533d0b2e35363c225f637ddbc316a5559
NTLuka <noobtenluka@gmail.com>
2021-12-21 15:33:25 +0100

    day 21 part 2 works but can be further optimized

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,6 @@
 use std::{fs, path::Path, time::Instant};
 
+use cached::proc_macro::cached;
 use itertools::Itertools;
 
 pub fn run() {
@@ -11,9 +12,15 @@ pub fn run() {
         "It took {:?} for the first solution to complete.",
         start.elapsed()
     );
+    let start = Instant::now();
+    solution_2(&inputs);
+    println!(
+        "It took {:?} for the second solution to complete.",
+        start.elapsed()
+    );
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Player {
     pub position: u32,
     pub score: u32,
@@ -63,38 +70,74 @@ fn solution_1(inputs: &(u32, u32)) {
     println!("The product is {}", number_of_rolls * losing_player_score);
 }
 
-fn solution_2(inputs: &(u32, u32)) {}
+fn solution_2(inputs: &(u32, u32)) {
+    let result = (3..=9).fold((0, 0), |acc, i| {
+        let output = solution_helper(
+            i,
+            true,
+            Player {
+                position: inputs.0,
+                score: 0,
+            },
+            Player {
+                position: inputs.1,
+                score: 0,
+            },
+        );
+        (acc.0 + output.0, acc.1 + output.1)
+    });
 
+    println!(
+        "The player with the most wins wins in {} universes",
+        result.0.max(result.1)
+    );
+}
+
+#[cached]
 fn solution_helper(
-    outcomes: &[u32],
-    mut is_player_ones_turn: bool,
-    mut player_1: Player,
-    mut player_2: Player,
+    sum_of_rolls: u32,
+    is_player_ones_turn: bool,
+    player_1: Player,
+    player_2: Player,
 ) -> (u64, u64) {
-    let vec: Vec<u32> = Vec::new();
-    let mut outcome_wrapper: Vec<u32> = Vec::from(outcomes);
+    let mut player_1_redec = player_1.clone();
+    let mut player_2_redec = player_2.clone();
     let player_ref = if is_player_ones_turn {
-        &mut player_1
+        &mut player_1_redec
     } else {
-        &mut player_2
+        &mut player_2_redec
     };
 
-    if outcomes.len() == 3 {
-        player_ref.score += outcomes.iter().fold(0, |acc, x| acc + x);
-        is_player_ones_turn = !is_player_ones_turn;
-        outcome_wrapper = vec;
-    }
+    player_ref.position = (player_ref.position + sum_of_rolls - 1) % 10 + 1;
+    player_ref.score += player_ref.position;
 
     if player_ref.score >= 21 {
         if is_player_ones_turn {
-            return (1, 0);
+            return (get_factor(sum_of_rolls), 0);
         } else {
-            return (0, 1);
+            return (0, get_factor(sum_of_rolls));
         }
     }
 
-    solution_helper(&outcome_wrapper, is_player_ones_turn, player_1, player_2);
-    (0, 0)
+    (3..=9).fold((0, 0), |acc, i| {
+        let output = solution_helper(i, !is_player_ones_turn, player_1_redec, player_2_redec);
+        (
+            acc.0 + output.0 * get_factor(sum_of_rolls),
+            acc.1 + output.1 * get_factor(sum_of_rolls),
+        )
+    })
+}
+fn get_factor(input: u32) -> u64 {
+    match input {
+        3 => 1,
+        4 => 3,
+        5 => 6,
+        6 => 7,
+        7 => 6,
+        8 => 3,
+        9 => 1,
+        _ => 0,
+    }
 }
 
 fn read_inputs<T: AsRef<Path>>(path: T) -> (u32, u32) {
